4fe3ff2a1bd11452b1ca73205dd59c593a3691ff
Rafael-Albernaz-dev <rafaelalbernaz6@gmail.com>
2020-01-09 20:55:05 -0300

    Add logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function calculate(type, value) {
+    var inputElement = document.querySelector('.result');
+    if (type === 'action') {
+        if (value === 'c') {
+            inputElement.value = '';
+        }
 
+        if (value === '*' || value === '+' || value === '/' || value === '-' || value === '.') {
+            inputElement.value += value;
+        }
+
+        if (value === '=') {
+            var equal = eval(inputElement.value);
+            inputElement.value = equal;
+        }
+    } else if (type === 'value') {
+        inputElement.value += value;
+    }
 }
